0b363846d67c6b9e0ad68c1e550e5ce589d1ea9f
Frizzell-Carlton, Russ <russ.frizzell-carlton@ge.com>
2025-01-05 13:09:45 -0500

    Changed neovim setup.

--- dot_config/private_zsh/private_custom/private_neovim.zsh
+++ dot_config/private_zsh/private_custom/private_neovim.zsh
@@ -1,10 +1,9 @@
 OHOST="$(hostname)"
+export NVIM_APPNAME=anvim
 if [[ "$OHOST" == "GCGPP5J3E" || "$OHOST" == "fedoraremix" ]]; then
-    alias vim="NVIM_APPNAME=rnvim nvim"
-    alias vi="NVIM_APPNAME=rnvim nvim"
-    export EDITOR="NVIM_APPNAME=rnvim nvim"
-else
-    alias vim="NVIM_APPNAME=anvim nvim"
-    alias vi="NVIM_APPNAME=anvim nvim"
-    export EDITOR="NVIM_APPNAME=anvim nvim"
+    export NVIM_APPNAME=rnvim
 fi
+
+alias vim=nvim
+alias vi=nvim
+export EDITOR=nvim
