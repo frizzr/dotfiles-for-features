b7bfd6ac2926d53ceb2d1e9ddb83809ec68a0eae
Frizzell-Carlton, Russ <russ.frizzell-carlton@ge.com>
2025-01-08 13:44:10 -0500

    Removed unneeded dependency.

--- dot_config/private_rnvim/private_init.lua
+++ dot_config/private_rnvim/private_init.lua
@@ -60,7 +60,6 @@ require("lazy").setup({
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
 },
 { "ibhagwan/fzf-lua", dependencies = { "nvim-tree/nvim-web-devicons" },
